d243d9623dff9df3a2b73b2e4cb81df2466810b9
Abeera <iftikharabeera100@gmail.com>
2025-09-26 01:07:09 -0700

    Add files via upload

---
+++ data types and operators.R
@@ -0,0 +1,58 @@
+Hello ("R programmming")
+
+#variables in r programming 
+#assignment of variables 
+
+var1<-15
+var2=20
+23->var3
+
+#data type in r
+
+num<-15.8
+class(num)
+
+int<-18L
+class(int)
+
+log<-"TRUE"
+class(log)
+
+comp<-5+2i
+class(comp)
+
+#operators in r 
+
+#assignment operators
+#(= <- ->)
+
+#arithmetic operator 
+
+num1<-20
+num2<-25
+
+#relational operators 
+
+num1<-20
+num2<-25
+
+#logical operators
+
+log1<-TRUE
+log2<-FALSE
+
+log1&log2
+
+
+#vectors in r 
+vec1<-c(1,2,3)
+vec2<-c("a", "b", "c")
+vec3<-c(TRUE, FALSE)
+
+bag1<-c(1,TRUE, 2, FALSE)
+bag2<-c(1,"a", 2, "b")
+bag3<-c(1, "a", TRUE, "b", FALSE)
+
+#list in r
+l1<-list(1,"a", TRUE)
+l2<-listc(1, 2, 3),c("a", "b","c")
